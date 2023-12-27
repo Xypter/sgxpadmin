d3e593e20b03d9a5c4127fe3a00f754c2ee2b6dc
Xypter <xypter@gmail.com>
2023-12-27 15:52:54 -0300

    New controller for sprites

--- sgxpadmin/src/api/sprite/controllers/sprite.ts
+++ sgxpadmin/src/api/sprite/controllers/sprite.ts
@@ -1,7 +1,36 @@
+'use strict';
+
 /**
  * sprite controller
  */
 
-import { factories } from '@strapi/strapi'
+export {};
 
-export default factories.createCoreController('api::sprite.sprite');
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sprite.sprite', ({ strapi }) => ({
+    async find(ctx) {
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+  
+      const query = strapi.db.query('api::sprite.sprite');
+  
+      await Promise.all(
+        data.map(async (item, index) => {
+          const sprite = await query.findOne({
+            where: {
+              id: item.id,
+            },
+            populate: ['createdBy'],
+          });
+          
+          data[index].attributes.createdBy = {
+            role: sprite.createdBy.user,
+            username: sprite.createdBy.username,
+          };
+        })
+      );
+  
+      return { data, meta };
+    },
+  }));
